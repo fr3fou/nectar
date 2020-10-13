0d5bac6f82f16b407ad2833ede25039d20c6cd7e
Simo Aleksandrov <simo3003@me.com>
2020-10-13 21:17:19 +0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,28 +1,38 @@
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, TopNavigation } from '@ui-kitten/components';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  TopNavigation,
+} from "@ui-kitten/components";
+import React from "react";
+import { StatusBar, useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
-    <TopNavigation
-      title='Nectar'
-    />
-    <Text category='h1'>Hi!</Text>
-  </Layout>
+  <>
+    <TopNavigation title="Nectar" />
+    <Layout
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>h</Text>
+    </Layout>
+  </>
 );
 
 export default function App() {
-  const colorScheme = useColorScheme()
+  const colorScheme = useColorScheme();
 
   return (
     <ApplicationProvider {...eva} theme={eva[colorScheme!]}>
       <SafeAreaProvider>
         <HomeScreen />
-        <StatusBar />
       </SafeAreaProvider>
+      <StatusBar translucent={false} />
     </ApplicationProvider>
-  )
-};
+  );
+}
