2ffd8781b7267cedb92727c59232345b54b47e3c
Simo Aleksandrov <simo3003@me.com>
2020-10-13 20:58:40 +0300

    dark mode

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, TopNavigation } from '@ui-kitten/components';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+    <TopNavigation
+      title='Nectar'
+    />
+    <Text category='h1'>Hi!</Text>
+  </Layout>
+);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const colorScheme = useColorScheme()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ApplicationProvider {...eva} theme={eva[colorScheme!]}>
+      <SafeAreaProvider>
+        <HomeScreen />
+        <StatusBar />
+      </SafeAreaProvider>
+    </ApplicationProvider>
+  )
+};
