9522e7de308ee8d772428f6886214b5f9ec33854
Simo Aleksandrov <simo3003@me.com>
2020-10-18 01:28:29 +0300

    return samples

--- main.go
+++ main.go
@@ -29,23 +29,39 @@ func main() {
 		panic(err)
 	}
 
-	parseSamples(pipe)
+	samples := parseSamples(pipe, 44100*5) // 5 secs of audio
 
-	if err := cmd.Wait(); err != nil {
+	if err := cmd.Process.Kill(); err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("output.pcm")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(samples)
+
+	if err := binary.Write(file, binary.LittleEndian, samples); err != nil {
 		panic(err)
 	}
 }
 
-// type Sample = float64
+type Sample = float32
 
-func parseSamples(r io.Reader) {
-	for {
+func parseSamples(r io.Reader, limit int) []Sample {
+	v := []Sample{}
+
+	for i := 0; i < limit; i += 44100 {
 		b := make([]byte, 32) // 32 because we're using float32
 		_, err := r.Read(b)
 		if err != nil {
 			log.Println(err)
+
 			break
 		}
-		fmt.Println(math.Float32frombits(binary.LittleEndian.Uint32(b)))
+		v = append(v, math.Float32frombits(binary.LittleEndian.Uint32(b)))
 	}
+
+	return v
 }
