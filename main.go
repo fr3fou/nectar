30fbc952309c6b5b502c243312a155c7e8fab0c2
Simo Aleksandrov <simo3003@me.com>
2020-10-17 19:16:09 +0300

    Parse samples (?)

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"math"
 	"os"
 	"os/exec"
 )
@@ -19,13 +24,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer pipe.Close()
 
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
 
+	parseSamples(pipe)
+
 	if err := cmd.Wait(); err != nil {
 		panic(err)
+	}
+}
+
+// type Sample = float64
+
+func parseSamples(r io.Reader) {
+	for {
+		b := make([]byte, 32) // 32 because we're using float32
+		_, err := r.Read(b)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		fmt.Println(math.Float32frombits(binary.LittleEndian.Uint32(b)))
 	}
 }
